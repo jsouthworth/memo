cadefc0c3bae3f1dccbf47693af40443353b6179
John Southworth <john.southworth@gmail.com>
2019-03-10 16:02:59 -0700

    test: add benchmarks and cleanup examples

--- memo_test.go
+++ memo_test.go
@@ -2,7 +2,7 @@ package memo
 
 import (
 	"fmt"
-	"time"
+	"testing"
 )
 
 func ExampleFib() {
@@ -22,13 +22,13 @@ func ExampleFib() {
 	}
 	memoFib := Memoize(fib)
 
-	start := time.Now()
 	out := memoFib(45)
-	fmt.Println(out, time.Since(start))
+	fmt.Println(out)
 
-	startCached := time.Now()
 	out = memoFib(45)
-	fmt.Println(out, time.Since(startCached))
+	fmt.Println(out)
+	// Output:1134903170
+	// 1134903170
 }
 
 func ExampleFibMemo() {
@@ -48,11 +48,96 @@ func ExampleFibMemo() {
 			memoFib(n-2).(int)
 	})
 
-	start := time.Now()
 	out := memoFib(45)
-	fmt.Println(out, time.Since(start))
+	fmt.Println(out)
 
-	startCached := time.Now()
 	out = memoFib(45)
-	fmt.Println(out, time.Since(startCached))
+	fmt.Println(out)
+	// Output:1134903170
+	// 1134903170
+}
+
+func BenchmarkFib(b *testing.B) {
+	var fib func(int) int
+	fib = func(n int) int {
+		if n == 0 {
+			return 0
+		}
+		if n == 1 {
+			return 1
+		}
+		return fib(n-1) + fib(n-2)
+	}
+	memoFib := Memoize(fib)
+	for i := 0; i < b.N; i++ {
+		memoFib(i % 45)
+	}
+
+}
+
+func BenchmarkMemoFib(b *testing.B) {
+	var memoFib func(...interface{}) interface{}
+	memoFib = Memoize(func(n int) int {
+		if n == 0 {
+			return 0
+		}
+		if n == 1 {
+			return 1
+		}
+		return memoFib(n-1).(int) +
+			memoFib(n-2).(int)
+	})
+
+	for i := 0; i < b.N; i++ {
+		memoFib(i % 45)
+	}
+
+}
+
+func BenchmarkFastFibRecursive(b *testing.B) {
+	var fibIter func(a, b, p, q, count int) int
+	fibIter = func(a, b, p, q, count int) int {
+		switch {
+		case count == 0:
+			return b
+		case count%2 == 0:
+			return fibIter(
+				a,
+				b,
+				(p*p)+(q*q),
+				(2*p*q)+(q*q),
+				count/2)
+		default:
+			return fibIter(
+				(b*q)+(a*q)+(a*p),
+				(b*p)+(a*q),
+				p,
+				q,
+				count-1)
+		}
+	}
+	fib := func(n int) int {
+		return fibIter(1, 0, 0, 1, n)
+	}
+	for i := 0; i < b.N; i++ {
+		fib(i % 45)
+	}
+}
+
+func BenchmarkFastFib(b *testing.B) {
+	fib := func(n int) int {
+		a, b, p, q, count := 1, 0, 0, 1, n
+		for count != 0 {
+			switch {
+			case count%2 == 0:
+				p, q, count = (p*p)+(q*q), (2*p*q)+(q*q), count/2
+			default:
+				a, b, count = (b*q)+(a*q)+(a*p), (b*p)+(a*q), count-1
+			}
+		}
+		return b
+	}
+	for i := 0; i < b.N; i++ {
+		fib(i % 45)
+	}
 }
